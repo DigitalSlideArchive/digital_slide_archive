9f2f1bfffa773e49bcc7bdb2ee30806927e34424
Brian Helba <brian.helba@kitware.com>
2016-04-08 01:29:20 -0400

    Fix a bug in ingest, handling an undocumented disease type, "lnnh"

--- server/datasets/tcga/ingest.py
+++ server/datasets/tcga/ingest.py
@@ -315,6 +315,9 @@ class TCGAIngest(Ingest):
 
         basePath = TCGAPath(self.BASE_URL)
         for diseaseStudyPath in self._listAutoIndex(basePath)[0]:
+            if diseaseStudyPath.diseaseStudyCode.upper() == 'LNNH':
+                # An undocumented disease study code
+                continue
             if diseaseStudyPath.diseaseStudyCode.upper() not in TcgaCodes.DISEASE_STUDIES:
                 raise IngestException('Unknown disease study: "%s"' % str(diseaseStudyPath))
 
