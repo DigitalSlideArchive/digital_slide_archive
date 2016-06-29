758b42259945097e761e54fb8b23919b86ff9b6c
Brian Helba <brian.helba@kitware.com>
2016-06-29 11:41:40 -0400

    Use homepage markdown plugin instead of customized static text

--- web_client/js/setup.js
+++ web_client/js/setup.js
@@ -1,19 +1,5 @@
 /* Wrap some views to show the brand name */
 
-girder.wrap(girder.views.FrontPageView, 'render', function (render) {
-    if (!girder.brandName) {
-        girder.brandName = $('title').text();
-    }
-    this.$el.html(girder.templates.frontPage({
-        apiRoot: girder.apiRoot,
-        staticRoot: girder.staticRoot,
-        currentUser: girder.currentUser,
-        versionInfo: girder.versionInfo,
-        brandName: girder.brandName
-    }));
-    return this;
-});
-
 girder.wrap(girder.views.LayoutHeaderView, 'render', function (render) {
     if (!girder.brandName) {
         girder.brandName = $('title').text();
