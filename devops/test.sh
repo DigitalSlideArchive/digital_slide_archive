93696068c7304f900794c9a31e097cf6f6cec0bd
David Manthey <david.manthey@kitware.com>
2020-03-20 09:13:33 -0400

    Fix bash parameter escaping as passed to docker.
    In order to preserve whitespace in parameters, this uses some arcane
    bash operations.  There is probably a better way to do it, but this
    appears to work in various tests.

--- devops/test.sh
+++ devops/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-var="$@"
+# @@Q quotes each parameter.  echo forms a single string that can be added to 
+# a command without further quoting.
+var=$(echo "${@@Q}")
 
-docker exec -it dsa_girder bash -lc "tox \"$var\""
+docker exec -it dsa_girder bash -lc "tox $var"
+
