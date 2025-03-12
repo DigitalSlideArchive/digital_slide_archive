56c81578ca9a4f0ba313d3b65cd4df9f65eec157
willdunklin <will.dunklin@kitware.com>
2025-03-12 12:25:45 -0400

    Update slicer_cli_web to use fork

--- devops/slurm/worker/create.sh
+++ devops/slurm/worker/create.sh
@@ -5,4 +5,4 @@ rm -rf ./lib/girder_worker ./lib/slicer_cli_web ./venv
 python -m venv ./venv
 
 git clone --branch slurm https://github.com/girder/girder_worker.git ./lib/girder_worker
-git clone --branch slicer-cli-web-singularity https://github.com/girder/slicer_cli_web.git ./lib/slicer_cli_web
+git clone --branch slicer-cli-web-singularity https://github.com/willdunklin/slicer_cli_web.git ./lib/slicer_cli_web
